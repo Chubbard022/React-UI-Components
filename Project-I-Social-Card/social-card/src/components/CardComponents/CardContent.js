33621a2ed7868ab110eaa2911ca37775f2ffabb8
chubbard022 <chubbard02@gmail.com>
2019-03-18 12:45:46 -0700

    added content to header container

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = ()=>{
+    return(
+        <div>
+            <h1>Get started with React</h1>
+            
+        </div>
+    )
+}
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderContent from './components/HeaderContent';
+import ImageThumbnail from './components/ImageThumbnail';
+import HeaderTitle from './components/HeaderTitle';
+
+
+const HeaderContainer = () =>{
+    return(
+    <div>
+    <ImageThumbnail />
+    <HeaderTitle />
+    <HeaderContent />
+    </div>
+    )
+}
+
+
+export default HeaderContainer;
